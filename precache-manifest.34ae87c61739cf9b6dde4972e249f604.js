b720f0843d8ff9969e1ac2fe45c4c513458cdf5f
OliinykKostya <oliinyk.kostya@gmail.com>
2020-05-08 13:16:38 +0300

    Updates

---
+++ precache-manifest.34ae87c61739cf9b6dde4972e249f604.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "581fa361c8e2ebf7b135f6670a48afd2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dbe5b77298961af83474",
+    "url": "./static/css/main.e56c2ea1.chunk.css"
+  },
+  {
+    "revision": "61556394945a0214de22",
+    "url": "./static/js/2.c4f2ff87.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.c4f2ff87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbe5b77298961af83474",
+    "url": "./static/js/main.c8512f6d.chunk.js"
+  },
+  {
+    "revision": "548aac5e4720dc7e1ea8",
+    "url": "./static/js/runtime-main.9c94e0be.js"
+  }
+]);
